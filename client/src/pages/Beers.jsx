3938a4e700db83ecd47cbf0621439b3d1b66baf6
Francis Bukenya <frankie.bukenya@gmail.com>
2020-05-22 14:22:55 +0200

    Revert

--- client/src/pages/Beers.jsx
+++ client/src/pages/Beers.jsx
@@ -20,7 +20,7 @@ class Beers extends Component {
         countryIsoCode: '',
       },
       name: '',
-      beers: [],
+      beersByName: [],
       // beersByType: [],
       beersByCountry: [],
       countryCode: [],
@@ -72,7 +72,7 @@ class Beers extends Component {
       .then((response) => {
         console.log(response);
         this.setState({
-          beers: response.data.beers,
+          beersByName: response.data.beers,
         });
       })
       .catch((err) => {
@@ -113,14 +113,14 @@ class Beers extends Component {
       });
   }
 
-  removeDuplicates() {
-    if (this.state.breweries) {
-      var unique = _.uniqBy(this.state.breweries, 'breweryId');
-    }
-    this.setState({
-      breweries: unique,
-    });
-  }
+  // removeDuplicates() {
+  //   if (this.state.breweries) {
+  //     var unique = _.uniqBy(this.state.breweries, 'breweryId');
+  //   }
+  //   this.setState({
+  //     breweries: unique,
+  //   });
+  // }
 
   render() {
     return (
@@ -129,7 +129,7 @@ class Beers extends Component {
           <div className="beers-heading">
             <h1 className="beers-title">Here's a list of our beers</h1>
           </div>
-          <div className="beers-search">
+          {/* <div className="beers-search">
             <form className="form-inline my-2 my-lg-0">
               <input
                 onChange={this.handleInputChange}
@@ -156,23 +156,6 @@ class Beers extends Component {
             </form>
 
             <div className="dropdown">
-              {/* <select
-                className="btn btn-outline-secondary"
-                aria-labelledby="dropdownMenuButton"
-                value={this.state.beersByType.toString()}
-                onClick={(e) => this.handleTypeChange(e)}
-              >
-                <option className="dropdown-item" defaultValue>
-                  Fiter by type
-                </option>
-
-                {this.state.locationType.map((type) => (
-                  <option name="locationType" key={type} value={type}>
-                    {type}
-                  </option>
-                ))}
-              </select> */}
-
               <select
                 className="btn btn-outline-secondary"
                 aria-labelledby="dropdownMenuButton"
@@ -183,20 +166,22 @@ class Beers extends Component {
                 <option className="dropdown-item" defaultValue>
                   Filter by country
                 </option>
-
-                {this.state.countryCode.map((country) => (
-                  <option name="locationByType" key={country} value={country}>
-                    {country}
+                {this.state.countryCode.map((item) => (
+                  <option name="selectedCode" key={item} value={item}>
+                    {item}
                   </option>
                 ))}
               </select>
             </div>
-          </div>
-          {this.state.beers ? (
+          </div> */}
+
+          {this.state.beersByName ? (
             <div className="beers-container container">
-              {this.state.beers.map((beer) => (
+              {this.state.beersByName.map((beer) => (
                 <div key={beer.id} className="beers-link-item">
-                  {beer.name ? (
+                  {beer.name
+                    .toLowerCase()
+                    .includes(this.state.name.toLowerCase()) ? (
                     <div>
                       <Link
                         to={`/beers/${beer.id}`}
@@ -216,33 +201,34 @@ class Beers extends Component {
               Whoops the fridge is empty, try another name
             </h4>
           )}
+
+          {this.state.beersByCountry ? (
+            <div className="beers-container container">
+              {this.state.beersByCountry.map((country) => (
+                <div key={country.id} className="beers-link-item">
+                  {country.breweries[0].locations[0].countryIsoCode.includes(
+                    this.state.select.countryIsoCode
+                  ) ? (
+                    <div>
+                      <Link
+                        to={`/beers/${country.id}`}
+                        className="beers-link-item"
+                      >
+                        <h5 className="beers-name">{country.name}</h5>
+                      </Link>
+                    </div>
+                  ) : (
+                    <p className="beers-not-found">That beer does not exists</p>
+                  )}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <h4 className="beers-error">
+              Whoops the fridge is empty, try another name
+            </h4>
+          )}
         </div>
-        {this.state.beersByCountry ? (
-          <div className="beers-container container">
-            {this.state.beersByCountry.map((country) => (
-              <div key={country.id} className="beers-link-item">
-                {country.breweries[0].locations[0].countryIsoCode.includes(
-                  this.state.select.selectedCode
-                ) ? (
-                  <div>
-                    <Link
-                      to={`/beer/${country.id}`}
-                      className="beers-link-item"
-                    >
-                      <h5 className="beers-name">{country.name}</h5>
-                    </Link>
-                  </div>
-                ) : (
-                  <p className="beers-not-found">That beer does not exists</p>
-                )}
-              </div>
-            ))}
-          </div>
-        ) : (
-          <h4 className="beers-error">
-            Whoops the fridge is empty, try another name
-          </h4>
-        )}
       </Default>
     );
   }
